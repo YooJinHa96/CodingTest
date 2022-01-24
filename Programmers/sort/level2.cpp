e70d3075f97e867aafc6aada9b682b114e3e82c9
YooJinHa96 <yjs19162@hanmail.net>
2022-01-24 18:30:23 +0900

    level2

--- Programmers/sort/level2.cpp
+++ Programmers/sort/level2.cpp
@@ -3,32 +3,35 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-
+bool is_digit(string str) {
+    return atoi(str.c_str()) != 0 || str.compare("0") == 0;
+}
 bool compare(string s1,string s2){
     if(s1[0]!=s2[0]){
         return s1>s2;
     }
     else if(s1.length()!=s2.length()){
-        
         if(s1.length()<s2.length()){
             for(int i=1;i<s2.length();i++){
-                    if(s1[0]<s2[i]){
-                        return s1>s2;
+                    if(s1[s1.size()-1]<s2[i]){
+                        return false;
                     }
-                    else if(s1[0]>s2[i]){
-                        return s1<s2;
+                    else if(s1[s1.size()-1]>s2[i]){
+                        return true;
                     }
             }
+            return true;
         }
         else if(s1.length()>s2.length()){
             for(int i=1;i<s1.length();i++){
-                    if(s2[0]<s1[i]){
-                        return s1>s2;
+                    if(s1[i]>s2[s2.size()-1]){
+                        return true;
                     }
-                    else if(s2[0]>s1[i]){
-                        return s1<s2;
+                    else if(s1[i]<s2[s2.size()-1]){
+                        return false;
                     }
             }
+            return false;
         }
     }
     
@@ -50,9 +53,20 @@ string solution(vector<int> numbers) {
 
     for(auto str : string_numbers)
          answer+=str;
-    return answer;
+    
+    for(auto a : answer){
+        if(a!='0'){
+            return answer;
+        }
+
+    }
+    return "0";
 }
+
 int main(){
-    string answer=solution({3,31,320, 30, 34, 5, 9});
-    cout<<answer<<'\n';
+    //string answer=solution({3,31,343,323,32,332,301,1000, 300,30,34, 100,10,5, 9,01});
+    string answer=solution({123,12,132,13,32,123});
+    //string answer=solution({1,0,1,0});
+ cout<<answer<<'\n';
+    
 }
